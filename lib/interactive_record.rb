fe2086acfffd909c5d0116d2d133b9eec42bde25
M Banks <montensia1@outlook.com>
2019-06-21 16:56:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,9 +49,9 @@ class InteractiveRecord
   end
   
   def self.find_by(attr_hash)
-    sql = ""
+    sql = []
     attr_hash.each do |key, value|
-    sql = "SELECT * FROM #{self.table_name} WHERE '#{key}' = '#{value}'"
+    sql << "SELECT * FROM #{self.table_name} WHERE '#{key}' = '#{value}'"
    
     DB[:conn].execute(sql)
     end
