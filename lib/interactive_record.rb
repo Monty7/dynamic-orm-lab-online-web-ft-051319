33c31a6d5be7444efca934644c80f0202611ddae
M Banks <montensia1@outlook.com>
2019-06-21 14:49:14 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,10 +52,10 @@ class InteractiveRecord
     attr_hash.each do |key, value|
     #binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{key} = #{value}"
-    end
+    
     # binding.pry
     DB[:conn].execute(sql)
-   
+   end
   end
   
   def save
